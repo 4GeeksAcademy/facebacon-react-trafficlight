4dd5a2eacf4e70f413cf01cd9f56da405f2e09d5
Sebastian Diaz <diazebas@gmail.com>
2024-08-20 00:49:22 +0000

    Created the super cool traffic light and I gave it a nice reset button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+	const resetColor = () => {setColor("");};
+    
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<div className="trafficLightContainer">
+		<div className="trafficLight">
+			<div 
+				className={color === 'red' ? 'red light glow' : 'red light'} 
+				onClick={() => setColor('red')}>
+			</div>
+			<div 
+				className={color === 'yellow' ? 'yellow light glow' : 'yellow light'} 
+				onClick={() => setColor('yellow')}>
+			</div>
+			<div 
+				className={color === 'green' ? 'green light glow' : 'green light'} 
+				onClick={() => setColor('green')}>
+			</div>
+			<button onClick={resetColor}>Reset</button>
 		</div>
-	);
+	</div>
+    );
 };
 
 export default Home;
